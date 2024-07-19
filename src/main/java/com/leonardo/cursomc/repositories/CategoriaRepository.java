4e77e48d91f6b04e2a361c055c60802ae78ddf9e
Leonardo <leo20yy@gmail.com>
2024-07-19 14:44:19 -0400

    Criando repository e service para categoria

---
+++ src/main/java/com/leonardo/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.leonardo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leonardo.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> { // tipo do repositório e o tipo di id
+
+	
+}
