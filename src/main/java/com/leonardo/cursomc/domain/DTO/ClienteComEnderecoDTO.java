8dd6b53721d0c68416f88e224dc180b7a76caad3
Leonardo-almeida22 <leo20yy@gmail.com>
2025-01-11 13:54:52 -0400

    Criando ClienteComEnderecoDTO e EnderecoDTO para cadastrar cliente com seu respectivo endereço em umúnico POST

---
+++ src/main/java/com/leonardo/cursomc/domain/DTO/ClienteComEnderecoDTO.java
@@ -0,0 +1,51 @@
+package com.leonardo.cursomc.domain.DTO;
+
+public class ClienteComEnderecoDTO {
+    private String nome;
+    private String email;
+    private String cpfOuCnpj;
+    private Integer tipo;
+
+    public ClienteDTO(){}
+    public ClienteDTO(Integer id, String nome, String email, String cpfOuCnpj, Integer tipo) {
+        this.nome = nome;
+        this.email = email;
+        this.cpfOuCnpj = cpfOuCnpj;
+        this.tipo = tipo;
+
+
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpfOuCnpj() {
+        return cpfOuCnpj;
+    }
+
+    public void setCpfOuCnpj(String cpfOuCnpj) {
+        this.cpfOuCnpj = cpfOuCnpj;
+    }
+
+    public Integer getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Integer tipo) {
+        this.tipo = tipo;
+    }
+}
---
+++ src/main/java/com/leonardo/cursomc/domain/DTO/EnderecoDTO.java
@@ -0,0 +1,80 @@
+package com.leonardo.cursomc.domain.DTO;
+
+public class EnderecoDTO {
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cep;
+
+    private Integer cidadeId;
+    private Integer clienteId;
+
+    public EnderecoDTO(){}
+
+    public EnderecoDTO(Integer id, String logradouro, String numero, String complemento, String bairro, String cep, Integer cidadeId, Integer clienteId) {
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.cidadeId = cidadeId;
+        this.clienteId = clienteId;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Integer getCidadeId() {
+        return cidadeId;
+    }
+
+    public void setCidadeId(Integer cidadeId) {
+        this.cidadeId = cidadeId;
+    }
+
+    public Integer getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(Integer clienteId) {
+        this.clienteId = clienteId;
+    }
+}
